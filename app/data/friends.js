dddb4bcbcb9726b3822d441de2d8ee5a61889a89
rmxjohnson <rhondaxjohnson@gmail.com>
2018-07-23 18:22:57 -0400

    try again

---
+++ app/data/friends.js
@@ -0,0 +1,72 @@
+
+//array to hold data for 'friends' that have taken the survey
+//initialize with data
+var friendList = [
+    {
+        name: 'Suzy Smith',
+        photo: 'https://images.pexels.com/photos/898978/pexels-photo-898978.jpeg?auto=compress&cs=tinysrgb&h=350',
+        scores: [
+            "4",
+            "5",
+            "5",
+            "2",
+            "4",
+            "5",
+            "4",
+            "4",
+            "3",
+            "4"
+        ]
+    },
+    {
+        name: 'Bobby Baldwin',
+        photo: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&h=350',
+        scores: [
+            "4",
+            "1",
+            "2",
+            "2",
+            "4",
+            "3",
+            "4",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        name: 'Daisy Duke',
+        photo: 'https://i.pinimg.com/736x/4c/f1/98/4cf198b6247791dfd38ff894b7a8aa79--short-denim-short-shorts.jpg',
+        scores: [
+            "5",
+            "2",
+            "2",
+            "1",
+            "4",
+            "3",
+            "3",
+            "2",
+            "1",
+            "4"
+        ]
+    },
+    {
+        name: 'Daffy Duck',
+        photo: 'https://vignette.wikia.nocookie.net/villains/images/4/45/Daffy_Duck_Official.png/revision/latest?cb=20170714013403',
+        scores: [
+            "2",
+            "1",
+            "1",
+            "2",
+            "2",
+            "1",
+            "1",
+            "2",
+            "2",
+            "1"
+        ]
+    }
+]
+
+// Export the array. This makes it accessible to other files using require.
+module.exports = friendList;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,79 @@
+// ===============================================================================
+// LOAD friendList DATA
+// ===============================================================================
+var friends = require('../data/friends.js');
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function (app) {
+    // API GET Request
+    // Below code handles when users "visit" a page.
+    // When a user visits a link
+    // (ex: localhost:PORT/api/friends... they are shown a JSON of the data in the table)
+    // ---------------------------------------------------------------------------
+
+    // Displays all friends in the friendList array
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+    });
+
+    // API POST Requests
+    // Below code handles when a user submits a form and thus submits data to the server.
+    // In each of the below cases, when a user submits form data (a JSON object)
+    // ...the JSON is pushed to the appropriate JavaScript array
+    // (ex. User fills out a survey request... this data is then sent to the server...
+    // Then the server saves the data to the friends array)
+    // ---------------------------------------------------------------------------
+
+    // Create New Friend - takes in JSON input
+    app.post("/api/friends", function (req, res) {
+        // req.body hosts is equal to the JSON post sent from the user
+        // This works because of our body-parser middleware
+        var newfriend = req.body;
+        // scores array for new friend (req parameter)
+        var newScores = req.body.scores;
+        // console.log("newscores = " + newScores);
+        var bestIndex = 0;
+        var bestCount = 9999;
+
+        // console.log(newfriend);
+        // console.log("friends" + friends + "friends.length = " + friends.length);
+
+        // find "best match" for the new friend
+        for (var i = 0; i < friends.length; i++) {
+            // console.log("bestCount = " + bestCount);
+            // console.log("current Scores = " + friends[i].scores);
+            var loopDiff = 0;
+            for (var j = 0; j < newScores.length; j++) {
+                loopDiff = loopDiff + Math.abs(newScores[j] - friends[i].scores[j]);
+                // console.log("loopDiff = " + loopDiff);
+            }
+            // console.log("Final Loop Diff = " + loopDiff);
+            if (loopDiff < bestCount) {
+                bestIndex = i;
+                bestCount = loopDiff;
+                // console.log("new best friend = " + i);
+            }
+
+            // console.log("name = " + friends[i].name);
+            // console.log("photo = " + friends[i].photo);
+            // console.log("scores = " + friends[i].scores);
+        }
+        var bestfriend = friends[bestIndex];
+        // console.log("bestfriend name = " + bestfriend.name);
+        // console.log("bestfriend photo = " + bestfriend.photo);
+        // console.log("bestfriend scores = " + bestfriend.scores);
+        // console.log("friendlist" + friends.friendList);
+        // console.log("friends" + friends);
+
+        // add new entry to friends array
+        friends.push(newfriend);
+
+        // return the best friend object to be displayed in the modal on survey page
+        res.json(bestfriend);
+    });
+
+}
